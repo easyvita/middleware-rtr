0bebd82a6061eca7c2ae121b29eeeae9eab878b0
easyvita <easyvita24@gmail.com>
2025-12-05 11:58:54 +0100

    server.js aktualisiert: Warmup entfernt, Free Render optimiert

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());           
-app.use(express.json());    
-
-app.get("/warmup", (req, res) => {
-  console.log("Warmup-Request – Server ist wach.");
-  res.send("OK");
-});
+app.use(cors());
+app.use(express.json()); 
 
 // Beispiel: Providers
 const providers = [
